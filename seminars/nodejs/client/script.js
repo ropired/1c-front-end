36b885b5131eafdb88a85ddbb1243fb38610d949
Misha <student2@UC.local>
2017-02-18 13:40:15 +0300

    18.02 server

---
+++ seminars/nodejs/client/script.js
@@ -0,0 +1,16 @@
+var button = document.getElementByID("request");
+button.addEventListener("click", function(){
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", "http://localhost:591/", true);
+  xhr.onreadystatechange = function() {
+    if(this.readyState == this.DONE) {
+      if(this.status != 200) {
+        console.log("Ошибка: " + this.status);
+      } else {
+        console.log(this.responseText);
+      }
+    }
+  }
+
+  xhr.send();
+});
--- seminars/nodejs/server/index.js
+++ seminars/nodejs/server/index.js
@@ -1,8 +1,11 @@
 var express = require("express");
+var cors = require("cors")
 var app = express();
 
+app.use(cors())
+
 app.get("/", function(request, response) {
-  response.send("Hello, Nodee.js!");
+  response.send("Hello, Node.js!");
 });
 
 app.listen(591);
